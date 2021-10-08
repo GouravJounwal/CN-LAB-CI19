ce9977087e19c981dc2b1f7019bba8a626bc1180
GouravJounwal <76249754+GouravJounwal@users.noreply.github.com>
2021-10-08 16:32:09 +0530

    Create bit_shiffting.cpp

---
+++ bit_shiffting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   unsigned char a=3,b=2;
+    std::cout << (a<<1) << std::endl;
+    std::cout << (b<<1) << std::endl;
+    std::cout << (a>>1) << std::endl;
+    std::cout << (b>>1) << std::endl;
+
+    return 0;
+}
